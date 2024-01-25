ebbb30bbf360e81b5a6f27d5a0438e369711ded9
tony221004 <tony@tappytoon.com>
2024-01-26 00:31:06 +0900

    chore: Init project

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'prettier'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'react/jsx-filename-extension': [1, { "extensions": ['.js', '.jsx', '.ts', '.tsx'] }],
+    'no-shadow': 'off',
+    'react/jsx-one-expression-per-line': 0,
+    'semi': ['off']
+  },
+}
